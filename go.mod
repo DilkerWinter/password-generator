8e3111cc55715251f70bf84e7a2b0d1a1f3477e6
Bruno Winter <brunohdwinter@gmail.com>
2024-12-30 20:57:30 -0400

    Creating connection to database

---
+++ go.mod
@@ -0,0 +1,12 @@
+module password-generator
+
+go 1.23.4
+
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
---
+++ main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"bufio"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"os"
+	"os/exec"
+	"password-generator/database"
+	"strconv"
+	"strings"
+
+	"github.com/atotto/clipboard"
+)
+
+func passwordToDatabase(password string, passwordName string) error {
+	db, err := database.Database()
+	if err != nil {
+		return fmt.Errorf("Could not connect to database : %v", err)
+	}
+	defer db.Close()
+
+	query := "INSERT INTO passwords (password_name, password) VALUES ($1, $2)"
+	_, err = db.Exec(query, passwordName, password)
+	if err != nil{
+		return fmt.Errorf("could not insert password: %v", err)
+	}
+
+	return nil
+}
+
+func addPassword() {
+	fmt.Printf("\n\nEnter the desired length for the password (number only): ")
+	var lengthInput string
+	fmt.Scan(&lengthInput)
+
+	length, err := strconv.Atoi(lengthInput)
+	if err != nil || length <= 0 {
+		fmt.Println("Invalid length.")
+		return
+	}
+
+	fmt.Printf("Should the password include uppercase letters? (y/n): ")
+	var upperCaseLetters string
+	fmt.Scan(&upperCaseLetters)
+
+	upperCaseLetters = strings.ToLower(upperCaseLetters)
+
+	var includeUpperCaseLetters bool
+	if upperCaseLetters == "y" || upperCaseLetters == "yes" {
+		includeUpperCaseLetters = true
+	} else if upperCaseLetters == "n" || upperCaseLetters == "no" {
+		includeUpperCaseLetters = false
+	} else {
+		fmt.Println("Invalid input, please enter 'y', 'yes', 'n' or 'no'.")
+		return
+	}
+
+	fmt.Printf("Should the password include special characters? (y/n): ")
+	var specialCharsInput string
+	fmt.Scan(&specialCharsInput)
+	
+	specialCharsInput = strings.ToLower(specialCharsInput)
+
+	var includeSpecialChars bool
+	if specialCharsInput == "y" || specialCharsInput == "yes" {
+		includeSpecialChars = true
+	} else if specialCharsInput == "n" || specialCharsInput == "no" {
+		includeSpecialChars = false
+	} else {
+		fmt.Println("Invalid input, please enter 'y', 'yes', 'n' or 'no'.")
+		return
+	}
+
+	fmt.Printf("What is the purpose or name for this password? ")
+	var passwordName string
+	fmt.Scan(&passwordName)
+
+	password := passwordGenerator(length, includeSpecialChars,includeUpperCaseLetters, passwordName)
+
+	fmt.Printf("Generated Password: %s\n", password)  
+
+	fmt.Printf("Wish to copy the password to clipboard? (y/n): ")
+	var clipboardInput string
+	fmt.Scan(&clipboardInput)
+
+	clipboardInput = strings.ToLower(clipboardInput)
+
+	var copyToClipboard bool
+	if clipboardInput == "y" || clipboardInput == "yes" {
+		copyToClipboard = true
+	} else if clipboardInput == "n" || clipboardInput == "no" {
+		copyToClipboard = false
+	} else {
+		fmt.Println("Invalid input, please enter 'y', 'yes', 'n' or 'no'.\n")
+		return
+	}
+
+	if copyToClipboard {
+		err := clipboard.WriteAll(password)
+		if err != nil {
+			fmt.Printf("Error copying to clipboard:\n", err)
+			return
+		}
+		fmt.Printf("Text copied to clipboard\n")
+	}
+
+	fmt.Println("Want to store that Password? (y/n): ")
+	var storePasswordInput string
+	fmt.Scan(&storePasswordInput)
+
+	storePasswordInput = strings.ToLower(storePasswordInput)
+
+	var storePassword bool
+	if storePasswordInput == "y" || storePasswordInput == "yes" {
+		storePassword = true
+	} else if storePasswordInput == "n" || storePasswordInput == "no" {
+		storePassword = false
+	} else {
+		fmt.Println("Invalid input, please enter 'y', 'yes', 'n' or 'no'.\n")
+		return
+	}
+
+	if storePassword {
+		err := passwordToDatabase(password, passwordName)
+		if err != nil {
+			fmt.Printf("Error storing password in database: %v\n", err)
+		} else {
+			fmt.Println("Password stored in database successfully.")
+			return
+		}
+	}
+	return
+	
+}
+
+func passwordGenerator(length int,includeSpecialChar bool,includeUpperCaseLetters bool, passwordName string) string {
+	
+	uppercase := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowercase := "abcdefghijklmnopqrstuvwxyz"
+	numbers := "0123456789"
+	specialChars := "!@#$%^&*()-_=+[]{}|;:,.<>?/~"
+
+	characters := lowercase + numbers
+	if includeSpecialChar {
+		characters += specialChars
+	}
+	if includeUpperCaseLetters {
+		characters += uppercase
+	}
+
+	var password []rune
+	for i := 0; i < length; i++ {
+		randomIndex, _ := rand.Int(rand.Reader, big.NewInt(int64(len(characters))))
+		password = append(password, rune(characters[randomIndex.Int64()]))
+	} 
+
+	return string(password)
+
+}
+
+func clearTerminal() {
+	cmd := exec.Command("clear")
+	if os.PathSeparator == '\\' {
+		cmd = exec.Command("cmd", "/c", "cls")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func main() {
+	clearTerminal()
+
+	fmt.Printf("----Password Generator---- \n\n")
+	fmt.Printf("[1] Generate New Password\n")
+	fmt.Printf("[2] Search Password\n")
+	fmt.Printf("[3] Exit\n")
+	fmt.Print("Choose an option: ")
+
+	reader := bufio.NewReader(os.Stdin)
+	optionStr, _, err := reader.ReadRune()
+
+	if err != nil {
+		fmt.Println("Error reading input")
+		return
+	}
+
+	switch optionStr {
+	case '1':
+		addPassword()
+	case '2':
+		fmt.Println("\nYou selected: Search Password")
+	case '3':
+		fmt.Println("Exiting...")
+		return
+	default:
+		fmt.Println("Invalid option. Please choose a valid option.")
+	}
+}
